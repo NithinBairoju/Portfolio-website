bdfd619e5112e0bf2616a7646a15e39b2e3085e3
NithinBairoju <nithinbairoju123@gmail.com>
2025-12-04 21:36:35 +0530

    fixed project  cards repsoniveness

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -54,7 +54,7 @@ const Projects = () => {
         </div>
 
         {/* Poster Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {projects.map((project, index) => (
             <a 
               key={index}
